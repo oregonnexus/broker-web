904ab6f4e867892ee4c78192c78f8dee021731f8
Ricky Ruiz <ricardo.ruiz@developers.net>
2023-09-17 16:44:49 -0700

    feat: enhance dashboard

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -2,12 +2,13 @@
 // Author: Makoa Jacobsen, makoa@makoajacobsen.com
 
 using System.Diagnostics;
+using InertiaAdapter;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OregonNexus.Broker.Web.Models;
 using OregonNexus.Broker.Domain;
 using OregonNexus.Broker.SharedKernel;
-using Microsoft.AspNetCore.Authorization;
-using InertiaAdapter;
+using OregonNexus.Broker.Web.Extensions.RequestStatuses;
 
 namespace OregonNexus.Broker.Web.Controllers;
 
@@ -15,18 +16,92 @@ namespace OregonNexus.Broker.Web.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IRepository<User> _userRepository;
+    private readonly IRepository<EducationOrganization> _educationOrganizationRepository;
+    private readonly IRepository<IncomingRequest> _incomingRequestRepository;
+    private readonly IRepository<OutgoingRequest> _outgoingRequestRepository;
 
-    private readonly IRepository<User> _repository;
-
-    public HomeController(ILogger<HomeController> logger, IRepository<User> repository)
+    public HomeController(
+        IRepository<User> userRepository,
+        IRepository<EducationOrganization> educationOrganizationRepository,
+        IRepository<IncomingRequest> incomingRequestRepository,
+        IRepository<OutgoingRequest> outgoingRequestRepository,
+        ILogger<HomeController> logger)
     {
+        _userRepository = userRepository;
+        _educationOrganizationRepository = educationOrganizationRepository;
+        _incomingRequestRepository = incomingRequestRepository;
+        _outgoingRequestRepository = outgoingRequestRepository;
         _logger = logger;
-        _repository = repository;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
     {
-        return View();
+        // TODO: Refactor and optimize dashboard queries, using dirty temporary ops for mockup purposes.
+
+        // IMPORTANT: Everything should be filtered by current Focus
+        // Some queries can be omitted based on user role.
+
+        // We will need to support a date range (start and end dates) to filter out the data.
+        // Currently displaying last 7 days, last 30 days, and All-time.
+        // To support All-time, the end date should be nullable.
+        // For example, Outgoing Processor does not need to see incoming transfer request data.
+
+        // Only take 5, displaying latest incoming requests
+        // Need the total count as well
+        var incomingRequests = (await _incomingRequestRepository.ListAsync(cancellationToken))
+            .OrderByDescending(incomingRequest => incomingRequest.CreatedAt)
+            .ToList();
+
+        // Only take 5, displaying latest outgoing requests
+        // Need the total count as well
+        var outgoingRequests = (await _outgoingRequestRepository.ListAsync(cancellationToken))
+            .OrderByDescending(outgoingRequest => outgoingRequest.CreatedAt)
+            .ToList();
+
+        var usersCount = await _userRepository.CountAsync(cancellationToken);
+
+        var organizations = (await _educationOrganizationRepository.ListAsync(cancellationToken))
+            .ToList();
+
+        // Temporary, taking 5 here
+        var incomingRequestViewModels = incomingRequests
+            .Take(5)
+            .Select(incomingRequest =>  new IncomingRequestViewModel()
+            {
+                Id = incomingRequest.Id,
+                District = incomingRequest.EducationOrganization?.ParentOrganization?.Name ?? string.Empty,
+                School = incomingRequest.EducationOrganization?.Name ?? string.Empty,
+                Student = incomingRequest.Student,
+                Date = incomingRequest.RequestDate,
+                Status = incomingRequest.RequestStatus.ToFriendlyString()
+            })
+            .ToList();
+
+        // Temporary, taking 5 here
+        var outgoingRequestViewModels = outgoingRequests
+            .Take(5)
+            .Select(outgoingRequest =>  new OutgoingRequestViewModel()
+            {
+                Id = outgoingRequest.Id,
+                District = outgoingRequest.EducationOrganization?.ParentOrganization?.Name ?? string.Empty,
+                School = outgoingRequest.EducationOrganization?.Name ?? string.Empty,
+                Student = outgoingRequest.Student,
+                Date = outgoingRequest.RequestDate,
+                Status = outgoingRequest.RequestStatus.ToFriendlyString()
+            })
+            .ToList();
+
+        var tempData = new DashboardViewModel()
+        {
+            InitialRequestsCount = incomingRequests.Count,
+            OutgoingRequestsCount = outgoingRequests.Count,
+            OrganizationsCount = organizations.Count,
+            UsersCount = usersCount,
+            LatestIncomingRequests = incomingRequestViewModels,
+            LatestOutgoingRequests = outgoingRequestViewModels,
+        };
+        return View(tempData);
     }
 
     public IActionResult Privacy()
---
+++ src/Models/DashboardViewModel.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using OregonNexus.Broker.Domain;
+using OregonNexus.Broker.Web.Constants.DesignSystems;
+
+namespace OregonNexus.Broker.Web.Models;
+
+public class DashboardViewModel
+{
+    [Display(Name = "Initial Requests Count")]
+    public int InitialRequestsCount { get; set; }
+
+    [Display(Name = "Outgoing Requests Count")]
+    public int OutgoingRequestsCount { get; set; }
+
+    [Display(Name = "Organizations Count")]
+    public int OrganizationsCount { get; set; }
+
+    [Display(Name = "Users Count")]
+    public int UsersCount { get; set; }
+
+    [Display(Name = "Latest Incoming Requests")]
+    public IEnumerable<IncomingRequestViewModel> LatestIncomingRequests { get; set; } = Enumerable.Empty<IncomingRequestViewModel>();
+
+    [Display(Name = "Latest Outgoing Requests")]
+    public IEnumerable<OutgoingRequestViewModel> LatestOutgoingRequests { get; set; } = Enumerable.Empty<OutgoingRequestViewModel>();
+}
