daa3a14d22465d03f69a4bb7798c99c1d6c07ca9
luisruiz <luis.ruiz@developers.net>
2023-09-17 22:16:31 -0700

    chore: add routing mapper

---
+++ src/Extensions/Routes/RouteExtension.cs
@@ -0,0 +1,28 @@
+﻿namespace OregonNexus.Broker.Web.Extensions.Routes
+{
+	public static class RouteExtension
+	{
+        public static void MapControllerRoutes(this IEndpointRouteBuilder endpoints, string routeName, string controllerName)
+        {
+            endpoints.MapControllerRoute(
+                name: $"{routeName}Index",
+                pattern: $"{routeName}",
+                defaults: new { controller = controllerName, action = "Index" });
+
+            endpoints.MapControllerRoute(
+                name: $"{routeName}Create",
+                pattern: $"{routeName}/add",
+                defaults: new { controller = controllerName, action = "Create" });
+
+            endpoints.MapControllerRoute(
+                name: $"{routeName}Update",
+                pattern: $"{routeName}/edit",
+                defaults: new { controller = controllerName, action = "Update" });
+
+            endpoints.MapControllerRoute(
+                name: $"{routeName}Delete",
+                pattern: $"{routeName}/delete",
+                defaults: new { controller = controllerName, action = "Delete" });
+        }
+    }
+}
