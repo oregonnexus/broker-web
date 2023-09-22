d71540725aca7d71ccda4ca092ad238cff78ee05
luisruiz <luis.ruiz@developers.net>
2023-09-21 21:08:35 -0700

    chore: incoming request display names

--- src/ViewModels/IncomingRequests/CreateIncomingRequestViewModel.cs
+++ src/ViewModels/IncomingRequests/CreateIncomingRequestViewModel.cs
@@ -15,23 +15,46 @@ public class CreateIncomingRequestViewModel
     [Required(ErrorMessage = "Student is required")]
     public string? Student { get; set; } = string.Empty;
 
+    [Display(Name = "SIS Number")]
     public string? SisNumber { get; set; }
 
+    [Display(Name = "EdFi ID")]
     public string? Id { get; set; }
+
+    [Display(Name = "Student Unique ID")]
     public string? StudentUniqueId { get; set; }
+
+    [Display(Name = "First Name")]
     public string? FirstName { get; set; }
+
+    [Display(Name = "Middle Name")]
     public string? MiddleName { get; set; }
+
+    [Display(Name = "Last Name")]
     public string? LastSurname { get; set; }
 
+    [Display(Name = "From District")]
     public string? FromDistrict { get; set; }
+
+    [Display(Name = "From School")]
     public string? FromSchool { get; set; }
+
+    [Display(Name = "From Email")]
     public string? FromEmail { get; set; }
 
+    [Display(Name = "To District")]
     public string? ToDistrict { get; set; }
+
+    [Display(Name = "To School")]
     public string? ToSchool { get; set; }
+
+    [Display(Name = "To Email")]
     public string? ToEmail { get; set; }
 
+    [Display(Name = "Note")]
     public string? Note { get; set; }
+
+    [Display(Name = "Contents")]
     public List<string>? Contents { get; set; }
 
     [Display(Name = "Request Status")]
